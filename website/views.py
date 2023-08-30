499593612df01c7a8ed137660361e720784b50f1
rafauoo <rafal5094@gmail.com>
2023-08-30 16:49:13 +0200

    Add login

--- website/views.py
+++ website/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+def home(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['passwd']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Zalogowano pomyślnie!")
+            return redirect('home')
+        else:
+            messages.success(request, "Nie udało się zalogować!")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
+
